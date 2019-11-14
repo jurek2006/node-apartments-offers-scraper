a87fc2235d3b21e8079debc6e3ae7d63cc444dee
Jurek <jurek2006@gmail.com>
2019-11-14 10:58:33 +0100

    scraping from two pages

--- scrap.js
+++ scrap.js
@@ -1,20 +1,8 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  page.setUserAgent(
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"
-  );
-  await page.goto("https://www.olx.pl/nieruchomosci/mieszkania/wynajem/olawa/");
-
-  //   await page.screenshot({ path: "example.png" });
-
-  //   get without promoted offers:
-  // Array.from(document.querySelectorAll("table.offers:not(.offers--top)"));
-
+const scrapOffersList = async page => {
   // get offers' list:
-  let offers = await page.evaluate(() =>
+  const offers = await page.evaluate(() =>
     Array.from(
       document.querySelectorAll(
         "table.offers:not(.offers--top) a.detailsLink:not(.thumb)"
@@ -22,6 +10,29 @@ const puppeteer = require("puppeteer");
     ).map(offer => ({ title: offer.innerText, href: offer.href }))
   );
   console.log(offers);
+};
 
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  page.setUserAgent(
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"
+  );
+  await page.goto("https://www.olx.pl/nieruchomosci/mieszkania/wynajem/olawa/");
+  console.log("pierwsza strona");
+  // await page.screenshot({ path: "example1.png" });
+
+  await scrapOffersList(page);
+
+  const nextPage = await page.evaluate(
+    () => document.querySelector('a[data-cy="page-link-next"]').href
+  );
+
+  console.log("go to: ", nextPage);
+
+  await page.goto(nextPage);
+  console.log("druga strona");
+  // await page.screenshot({ path: "example2.png" });
+  await scrapOffersList(page);
   await browser.close();
 })();
