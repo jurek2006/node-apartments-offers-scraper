851cfa50b703a2b3ac35bc333ecca2a9d4cd2ad4
Jurek <jurek2006@gmail.com>
2019-12-13 08:44:13 +0100

    added chalk for colors in console

--- utils/developerUtils.js
+++ utils/developerUtils.js
@@ -1,11 +1,12 @@
+const chalk = require('chalk');
 const isShowingVerboseLogsOn = true;
 
 exports.verboseLog = message => {
   if (isShowingVerboseLogsOn) {
-    console.log(`VERBOSE: ${message}`);
+    console.log(chalk.yellow(`VERBOSE: ${message}`));
   }
 };
 
 exports.userLog = message => {
-  console.log(message);
+  console.log(chalk.blue(message));
 };
